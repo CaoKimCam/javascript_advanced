a3c4e42a9082e43bc425c62858c12a5a67ffc967
CaoKimCam <21520641@gm.uit.edu.vn>
2024-03-15 09:44:29 +0700

    Hoisting

---
+++ lesson/hoisting.js
@@ -0,0 +1,9 @@
+// hosting với var, function declaration
+
+console.log(age)
+var age = 16;//chỉ đưa phần var age lên phần trên(khởi tạo giá trị undefined)
+
+// với function nó hoạt động bình thường trước
+// dù để hàm ở sau vẫn lấy được giá trị
+
+//let, const: được hoisting lên nhưng không tạo giá trị default
